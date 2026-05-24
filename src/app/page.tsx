19fbdda03e2fe93cf6690049c65bfc777ca840bf
shelod-eng <shelod@gmail.com>
2026-05-24 03:47:47 +0200

    Restore public homepage at root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-'use client';
-
 import HomepageLanding from './homepage/HomepageLanding';
+
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return <HomepageLanding />;
